515f0d90dc7a9985f26556a0eb8d98a76e493b83
jonathanlinwen <jonathanlinwen@gmail.com>
2025-04-08 18:45:59 +0800

    aaa
    aa

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ const element1 = document.getElementById("myBar1");
       document.getElementById("s01").addEventListener("change", refreshTime); 
       document.getElementById("s02").addEventListener("change", optionSel); 			  
       });
-		                              
+		// *******                              
           function refreshTime() {
              switch ( $(this).val()) {
 					  case "A": 
